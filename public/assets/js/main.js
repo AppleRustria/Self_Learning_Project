5799d1a717035de40a4ccec65f92f005dae47913
AppleRustria <arustria@myseneca.ca>
2024-12-07 03:15:49 -0500

    gsap execute 1
    simple

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -1,50 +1,45 @@
-/*------------------------------
-Register plugins
-------------------------------*/
-gsap.registerPlugin(ScrollTrigger, ScrollSmoother);
-const content = document.querySelector('#content');
+let animate = gsap.timeline();
 
-/*------------------------------
-Making some circles noise
-------------------------------*/
-const simplex = new SimplexNoise();
-for (let i = 0; i < 5000; i++) {
-  const div = document.createElement('div');
-  div.classList.add('circle');
-  const n1 = simplex.noise2D(i * 0.003, i * 0.0033);
-  const n2 = simplex.noise2D(i * 0.002, i * 0.001);
-  
-  const style = {
-    transform: `translate(${n2 * 200}px) rotate(${n2 * 270}deg) scale(${3 + n1 * 2}, ${3 + n2 * 2})`,
-    boxShadow: `0 0 0 .2px hsla(${Math.floor(i * 0.3)}, 70%, 70%, .6)`
-  };
-  Object.assign(div.style, style);
-  content.appendChild(div);
-}
-const Circles = document.querySelectorAll('.circle');
+animate.from('.bg',{
+    x:-1600,
+    duration:1,
+})
 
-/*------------------------------
-Init ScrollSmoother
-------------------------------*/
-const scrollerSmoother = ScrollSmoother.create({
-  content: content,
-  wrapper: '#wrapper',
-  smooth: 1,
-  effects: false
-});
+animate.from('.c1',{
+    y:-500,
+    duration:1,
+})
 
-/*------------------------------
-Scroll Trigger
-------------------------------*/
-const main = gsap.timeline({
-  scrollTrigger: {
-    scrub: 0.7,
-    start: "top 25%",
-    end: "bottom bottom"
-  }
-});
-Circles.forEach((circle) => {
-  main.to(circle, {
-    opacity: 1,
-  });
-});
+animate.from('.c2',{
+    y:1600,
+    duration:1,
+})
+
+animate.from('.img',{
+    x:-1600,
+    duration:1,
+})
+
+animate.from('.sc',{
+    x:1600,
+    duration:1,
+    opacity:0,
+})
+
+animate.from('h1',{
+    x:-60,
+    duration:.5,
+    opacity:0,
+})
+
+animate.from('p',{
+    x:-60,
+    duration:.5,
+    opacity:0,
+})
+
+animate.from('h2',{
+    x:-58,
+    duration:.5,
+    opacity:0,
+})
