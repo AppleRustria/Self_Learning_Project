530efa2493f6c5237ba76cc693aa2e1e870baa14
AppleRustria <arustria@myseneca.ca>
2024-12-07 23:55:44 -0500

    add more js libraries

--- public/assets/js/home.js
+++ public/assets/js/home.js
@@ -97,3 +97,24 @@ gsap.to(".num", {
     start: "right left",
   },
 });
+
+
+// for background
+
+
+var granimInstance = new Granim({
+    element: '#granim-canvas',
+    direction: 'left-right', // or 'top-bottom', 'radial', etc.
+    isPausedWhenNotInView: true,
+    stateTransitionSpeed: 500,
+    states : {
+      "default-state": {
+        gradients: [
+          ['#834D9B', '#D04ED6'],
+          ['#1CD8D2', '#93EDC7']
+        ],
+        transitionSpeed: 2500
+      }
+    }
+  });
+
